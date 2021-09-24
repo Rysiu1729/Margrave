e60d416a98f5d0d8555f5be02a0e1a11f012cc27
MistrzJavy <prezydent3rp@gmail.com>
2021-09-24 11:10:11 +0200

    Creating project

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.db import models
+from django.forms import TextInput, Textarea
+
+from game.models import Chapter
+from game.models import Section
+from game.models import Page
+from game.models import Option
+from game.models import Effect
+from game.models import Person
+
+
+
+
+class Options(admin.TabularInline):
+    model = Option
+    show_change_link = True
+
+
+class PageAdmin(admin.ModelAdmin):
+    inlines = [
+        Options
+    ]
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 40, 'cols': 100})},
+    }
+
+
+class Pages(admin.TabularInline):
+    model = Page
+    show_change_link = True
+
+
+class SectionsAdmin(admin.ModelAdmin):
+    inlines = [
+        Pages
+    ]
+
+class Sections(admin.TabularInline):
+    model = Section
+    show_change_link = True
+
+class ChapterAdmin(admin.ModelAdmin):
+    inlines = [
+        Sections
+    ]
+
+admin.site.register(Chapter,ChapterAdmin)
+admin.site.register(Section,SectionsAdmin)
+admin.site.register(Page,PageAdmin)
+admin.site.register(Option)
+admin.site.register(Effect)
+admin.site.register(Person)
+
--- game/models.py
+++ game/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Section(models.Model):
+    title = models.CharField(max_length=200)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Page(models.Model):
+    text = models.TextField(max_length=40000)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text[:100]+"..."
+
+
+
+class Effect(models.Model):
+    next_page = models.ForeignKey(Page,on_delete=models.CASCADE)
+    action = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.action
+
+
+
+class Option(models.Model):
+    text = models.CharField(max_length=10000)
+    page = models.ForeignKey(Page,on_delete=models.CASCADE)
+    effect = models.ForeignKey(Effect,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
+    relations = models.IntegerField(default=0)
+    position = models.CharField(max_length=200)
+    story = models.CharField(max_length=40000)
+
+    def __str__(self):
+        return self.name
